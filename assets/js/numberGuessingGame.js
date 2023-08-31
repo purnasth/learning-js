1d3288d81a70484cbc586b930bd6112b3cbf7d4f
Purna Shrestha <crush9918@gmail.com>
2023-08-31 19:21:36 +0545

    Create number guessing game project

---
+++ assets/js/numberGuessingGame.js
@@ -0,0 +1,22 @@
+console.log("\n\n");
+console.log("Day 6 Project I\n");
+console.log("Number Guessing Game\n");
+
+let userInput = window.prompt("Enter a number in between 1 and 3: ");
+
+userInput = Number(userInput);
+// console.log(typeof userInput);
+
+console.log("User input = ", userInput);
+
+let computerGuess = Math.round(Math.random() * 3);
+
+console.log("Computer guess = ", computerGuess);
+
+if (userInput === computerGuess) {
+  console.log("You guessed the correct number!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+console.log("\n\n");
