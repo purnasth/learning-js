cd5842f558454ee398bead28eea72513922ca0c3
Purna Shrestha <crush9918@gmail.com>
2023-08-31 19:07:46 +0545

    Learn ifelse

---
+++ ifElse.js
@@ -0,0 +1,22 @@
+console.log("\n\n");
+console.log("DAY 5 if else {}\n\n");
+
+console.log("I'm before if block.");
+
+if (true) {
+  console.log("This is true & I'm inside if block.");
+}
+console.log("I'm outside after if block.");
+
+let computerScore = 5;
+let myScore = 10;
+
+if (myScore > computerScore) {
+  console.log("I won!");
+} else if (myScore < computerScore) {
+  console.log("I lost!");
+} else {
+  console.log("It's a tie!");
+}
+
+console.log("\n\n");
