f2f5deb88d5b72fba6ab1774ced98a3c8ae1be50
Purna Shrestha <crush9918@gmail.com>
2023-08-31 18:59:40 +0545

    Learn operators

---
+++ operator.js
@@ -0,0 +1,18 @@
+console.log("\n\n");
+console.log("DAY 4 Operator\n\n");
+// == equal to
+// === equal value and equal data type
+// != not equal
+// !== not equal value or not equal data type
+
+let age = 21;
+let ageString = "21";
+
+console.log("age = ", age);
+console.log("ageString = ", ageString);
+console.log("age == ageString", age == ageString);
+console.log("age === ageString", age === ageString);
+console.log("age != ageString", age != ageString);
+console.log("age !== ageString", age !== ageString);
+
+console.log("\n\n");
