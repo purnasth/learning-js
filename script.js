ac5f89518568fea10b9c1d10fe23db7e0b23e5e3
Purna Shrestha <crush9918@gmail.com>
2023-08-31 10:46:27 +0545

    Initliaze; Learn random, round, floor and ceil function

---
+++ script.js
@@ -0,0 +1,48 @@
+// Random Numbers
+
+/*
+Math.random()
+Math.round()
+Math.floor()
+Math.ceil()
+*/
+
+//! Math.random()
+// let randomNumber = Math.random(); //? random number between 0 (including) and 1 (excluding)
+// console.log(randomNumber);
+// document.write(randomNumber);
+// document.write("<br>");
+// //? display only upto the 4th decimal place
+// randomNumber = randomNumber.toFixed(4);
+// document.write(randomNumber);
+
+//! Math.round()
+// //? Generate a random number between 0 (inclusive) and 10 (exclusive)
+// let randomNumber = Math.random() * 10;
+
+// console.log("Random number without rounding:", randomNumber);
+
+// //? Round the random number to the nearest integer
+// let roundedNumber = Math.round(randomNumber);
+
+// console.log("Random number after rounding:", roundedNumber);
+
+// ! Maths.floor()
+
+// let randomNumber = Math.random() * 10; //? random number between 0 (including) and 10 (excluding)
+
+// console.log("Random number without floor function:", randomNumber);
+
+// let randomNumberAfterFloor = Math.floor(randomNumber);
+
+// console.log("Random number after floor function:", randomNumberAfterFloor);
+
+// ! Maths.ceil()
+
+// let randomNumber = Math.random() * 10; //? random number between 0 (including) and 10 (excluding)
+
+// console.log("Random number without ceil function:", randomNumber);
+
+// let randomNumberAfterCeil = Math.ceil(randomNumber);
+
+// console.log("Random number after ceil function:", randomNumberAfterCeil);
