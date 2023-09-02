2222675dea5b689bb6731762dd751cd09a53354a
Purna Shrestha <crush9918@gmail.com>
2023-09-02 21:35:25 +0545

    Learn while loop

---
+++ assets/js/whileLoop.js
@@ -0,0 +1,10 @@
+console.log("\n\n");
+console.log("DAY 9 while loop\n\n");
+
+let counter = 0;
+while (counter <= 5) {
+  console.log("Counter = ", counter);
+  counter++;
+}
+
+console.log("\n\n");
