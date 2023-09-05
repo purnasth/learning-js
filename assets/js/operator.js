cfeafa9fc15c0e7fef485479bf6a60e88cf5c6c3
Purna Shrestha <crush9918@gmail.com>
2023-09-05 11:26:33 +0545

    Learn more operators

--- assets/js/operator.js
+++ assets/js/operator.js
@@ -16,3 +16,78 @@ console.log("age != ageString", age != ageString);
 console.log("age !== ageString", age !== ageString);
 
 console.log("\n\n");
+
+console.log("\nAll operators in detail\n\n");
+// = assignment operator
+// + addition operator
+// - subtraction operator
+// * multiplication operator
+// / division operator
+// % remainder operator
+// ** exponentiation operator
+// ++ increment operator
+// -- decrement operator
+// += addition assignment operator
+// -= subtraction assignment operator
+// *= multiplication assignment operator
+// /= division assignment operator
+// %= remainder assignment operator
+// **= exponentiation assignment operator
+// == equal to operator
+// === equal value and equal data type operator
+// != not equal operator
+// !== not equal value or not equal data type operator
+// > greater than operator
+// < less than operator
+// >= greater than or equal to operator
+// <= less than or equal to operator
+// ? ternary operator
+// && logical and operator
+// || logical or operator
+// ! logical not operator
+// typeof operator
+// delete operator
+// in operator
+// instanceof operator
+// void operator
+// , comma operator
+// new operator
+// this operator
+// super operator
+// class operator
+// extends operator
+// yield operator
+// async function operator
+// await operator
+// . dot operator
+// [] square bracket operator
+// () parentheses operator
+// {} curly braces operator
+// new operator
+// new.target operator
+// ... spread operator
+// ... rest operator
+// ? optional chaining operator
+// ?? nullish coalescing operator
+// ??= nullish coalescing assignment operator
+// => arrow function operator
+// function operator
+// function* operator
+// yield* operator
+// yield operator
+// async function operator
+// async function* operator
+// await operator
+// # private field operator
+// # private method operator
+// # private getter operator
+// # private setter operator
+// # private static field operator
+// # private static method operator
+// # private static getter operator
+// # private static setter operator
+// # private constructor operator
+// # private instance operator
+// # private static operator
+// # private prototype operator
+// # private static prototype operator
