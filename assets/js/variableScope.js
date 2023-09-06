aa53fe6b0477d80af1d8c1d10d0a3a684fec90a1
Purna Shrestha <crush9918@gmail.com>
2023-09-06 14:04:49 +0545

    Block scope concept

--- assets/js/variableScope.js
+++ assets/js/variableScope.js
@@ -14,3 +14,26 @@ console.log("Calling a global variable outside the block : ", userName);
 console.log("Calling a local variable outside the block : ", fullName); //shows error saying fullName is not defined.
 
 console.log("\n\n");
+
+console.log("block-scoped concept");
+// global variable
+let a = "Hello";
+
+function greet() {
+  // local variable
+  let b = "World";
+
+  console.log(a + " " + b);
+
+  if (b == "World") {
+    // block-scoped variable
+    let c = "hello";
+
+    console.log(a + " " + b + " " + c);
+  }
+
+  // variable c cannot be accessed here
+  console.log(a + " " + b + " " + c);
+}
+
+greet();
