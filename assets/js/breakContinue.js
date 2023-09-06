5666439cafa37051b099625a94c0c6b5c9c1699f
Purna Shrestha <crush9918@gmail.com>
2023-09-06 14:00:03 +0545

    Learn break and continue

---
+++ assets/js/breakContinue.js
@@ -0,0 +1,16 @@
+console.log("break; & continue; \n");
+
+//?break;
+// break is used to terminate the loop immediately when it is encountered.
+// Syntax:
+// break [label];
+// NOTE: label is optional and rarely used.
+
+
+//? continue;
+// continue is used to skip the current iteration of the loop and continue with the next iteration.
+// Syntax:
+// continue [label];
+// NOTE: label is optional and rarely used.
+
+//! NOTE: break; & continue; can be used with any loop or switch statement.
