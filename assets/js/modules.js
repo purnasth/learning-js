06984fc9a5a03d027ef75323137872d28672397e
Purna Shrestha <crush9918@gmail.com>
2023-09-10 13:40:44 +0545

    Learn default import and export

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -2,9 +2,15 @@ console.log("Learning modules...");
 
 import { sum, subtract } from "./modules/calc.js";
 import { greetPerson } from "./modules/greet.js";
+// import { default as sayName } from "./modules/sayName.js";
+// another method of importing default export
+import sayName from "./modules/sayName.js";
 
 console.log(sum(1, 2));
 console.log(subtract(1, 2));
 
 let displayName = greetPerson("Purna");
 console.log(displayName);
+
+let name = sayName("Purna");
+console.log(name);
---
+++ assets/js/modules/sayName.js
@@ -0,0 +1,5 @@
+const sayName = (name) => {
+  return `Hello, ${name}! from default export`;
+};
+
+export default sayName;
